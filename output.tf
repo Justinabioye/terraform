3a91edaaaba14ab96cf1d32780f8de039bedc37a
Justinabioye <101688769+Justinabioye@users.noreply.github.com>
2023-05-20 01:41:29 -0700

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,1 +1,15 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIA6IBDF7RQVLU63W44"
+  secret_key = "miENa4QkoUPhIaEWTUgNInsUXF6bTIUhZt2RTj50"
+}
 
+resource "aws_s3_bucket" "example" {
+  bucket = "redhatansibleajayanswere"
+
+  
+  }
+output "s3_bucket_arn" {
+
+  value= aws_s3_bucket.example
+}
