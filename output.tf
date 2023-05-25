30e433659fd3c2d691672a0c5c151546239ee974
Justinabioye <101688769+Justinabioye@users.noreply.github.com>
2023-05-25 03:58:56 -0700

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA6IBDF7RQVLU63W44"
-  secret_key = "miENa4QkoUPhIaEWTUgNInsUXF6bTIUhZt2RTj50"
 }
 
 resource "aws_s3_bucket" "example" {
